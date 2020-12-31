ae9df36caac74191f7b991ddedcdcc6dd9b78e85
Kasen <18170166+imkasen@users.noreply.github.com>
2020-12-31 21:18:35 +0800

    完成单页抓取测试

--- bv_crawler.py
+++ bv_crawler.py
@@ -2,7 +2,7 @@
 
 import requests
 import json
-
+import time
 
 id_tyyh = "2301165"  # 天翼羽魂 id
 search_keyword1 = "乃木坂工事中 坂道之诗"
@@ -28,7 +28,7 @@ params = {
 }
 
 proxies = {
-     "http": "153.36.134.176:9999"
+     "http": "115.221.246.148:9999"
 }
 
 response = requests.get(
@@ -39,5 +39,27 @@ response = requests.get(
 )
 
 results = response.json()
-total_page_num = int(results["data"]["page"]["count"] / results["data"]["page"]["ps"] + 1)
+total_page_num = int(results['data']['page']['count'] / results['data']['page']['ps'] + 1)
 print(total_page_num)
+
+bv_dict = {}
+for bv_info in reversed(results['data']['list']['vlist']):
+
+    video_bvid = str(bv_info['bvid'])                                       # bvid
+    video_title = bv_info['title']                                          # 标题
+    video_ep = video_title.split()[0].split('】')[1]                         # EP
+    video_created_time = time.asctime(time.localtime(bv_info['created']))   # 投稿时间
+    video_play = str(bv_info['play'])                                       # 播放数量
+    video_comment = str(bv_info['comment'])                                 # 评论数量
+    video_danmaku = str(bv_info['video_review'])                            # 弹幕数量
+
+    # 例子：
+    # {'BV1vt411k7At': ['EP187', '【乃木坂工事中】EP187 一期生四人联合毕业式【坂道之诗】', 'Mon Dec 24 07:54:13 2018', '96786', '684', '2578']}
+    bv_dict[video_bvid] = [video_ep, video_title, video_created_time, video_play, video_comment, video_danmaku]
+
+
+print(bv_dict)
+
+with open('resources/bv_info.json', 'w') as bv_file:
+    json.dump(bv_dict, bv_file, ensure_ascii=False, indent=2)
+
