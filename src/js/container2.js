c9f5406882360180740214aef8e61db0ff3f6ac7
Kasen <18170166+imkasen@users.noreply.github.com>
2021-03-27 13:24:37 +0800

    update style, import echarts-wordcloud 2
    update Promise
    
    update style

--- src/js/container2.js
+++ src/js/container2.js
@@ -21,286 +21,286 @@ myChart.showLoading();
 
 // 配置 ECharts
 export function drawChart2(data) {
-  myChart.hideLoading();
+    myChart.hideLoading();
 
-  let option = {
-    aria: { // 无障碍访问
-      enabled: true,
-    },
-    title: { // 标题
-      // text: "【乃木坂工事中】千葉幽羽部分视频数据",
-      subtext: "2018 年 12 月 ~ 至今",
-      left: 'center',
-    },
-    tooltip: { // 提示框
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross',
-      },
-      formatter: function (params) { // 自定义
-        /*
-        * 注意：
-        * params[0] 对应 series[0]，以此类推。
-        * 因为 series[0] 用了 data，
-        * 而 series[1|2] 用了 encode，
-        * 所以 params[0].value 返回 number，
-        * 而 params[1|2].value 返回 object。
-        */
-
-        let htmlStr = '';
-
-        htmlStr += params[1].name + "</br>"; // EP 数，与 X 轴相同
-        htmlStr += params[1].value['Title'] + "</br>"; // 标题
-        // htmlStr += params[1].value['Time'] + "</br>"; // 投稿时间
-
-        params.forEach((param) => {
-          htmlStr += param.marker + " " + param.seriesName + " : ";
-          if (param.value.constructor === Object) {
-            let param_key = Object.keys(param.value)[param.encode['y'][0]];
-            htmlStr += param.value[param_key];
-          }
-          else {
-            htmlStr += param.value;
-          }
-          htmlStr += "</br>";
-        });
-
-        return htmlStr;
-      }
-    },
-    toolbox: { // 工具栏
-      right: '15%',
-      feature: {
-        dataView: {
-          readOnly: true,
+    let option = {
+        aria: { // 无障碍访问
+            enabled: true,
         },
-        magicType: {
-          type: ['line', 'bar'],
+        title: { // 标题
+            // text: "【乃木坂工事中】千葉幽羽部分视频数据",
+            subtext: "2018 年 12 月 ~ 至今",
+            left: 'center',
         },
-        saveAsImage: {
-          name: "【乃木坂工事中】千葉幽羽部分视频数据",
-          pixelRatio: 1,
-          backgroundColor: { // 下载水印
-            type: 'pattern',
-            image: getWaterMark(),
-            repeat: 'repeat',
-          },
-        },
-        restore: {},
-      },
-    },
-    grid: { // 直角坐标系
-      right: '10%',
-      // containLabel: true,
-    },
-    legend: { // 图例
-      left: '10%',
-      data: ["播放次数", "弹幕数量", "评论数量"],
-    },
-    dataset: { // 数据集
-      source: data,
-    },
-    calculable: true,
-    xAxis: { // X 轴设置
-      // name: "期数",
-      type: 'category',
-      axisTick: {
-        alignWithLabel: true, // 刻度线与标签对齐
-      },
-      axisPointer: {
-        type: 'shadow',
-      },
-    },
-    yAxis: [ // Y 轴设置
-      {
-        name: "播放数",
-        type: 'value',
-        min: 0,
-        max: max_play,
-        interval: interval_play,
-        position: 'left',
-        axisLine: {
-          show: true, // 显示轴线
-        },
-        axisLabel: {
-          formatter: function (value) {
-            value = +value;
-            return isFinite(value) ? echarts.format.addCommas(+value / 1000) + 'K' : '';
-          }
-        },
-      },
-      {
-        name: "弹幕数",
-        type: 'value',
-        min: 0,
-        max: max_danmaku,
-        interval: interval_danmaku,
-        position: 'right',
-        axisLine: {
-          show: true,
-        },
-      },
-      {
-        name: "评论数",
-        type: 'value',
-        min: 0,
-        max: max_comment,
-        interval: interval_comment,
-        position: 'right',
-        offset: 70,
-        axisLine: {
-          show: true,
-        },
-      },
-    ],
-    dataZoom: [ // 缩放
-      {
-        type: 'slider',
-        show: true,
-        start: 0,
-        end: 10,
-      },
-      {
-        // type: 'inside',
-        // show: true,
-        // start: 0,
-        // end: 25,
-      }
-    ],
-    series: [
-      {
-        name: '播放次数',
-        type: 'line',
-        yAxisIndex: 0,
-        // encode: {
-        //   x: "EP",
-        //   y: "Play",
-        // },
-        data: data.map(function (item) {
-          return item["Play"];
-        }),
-        itemStyle: {  // 折线拐点样式
-          color: '#BA55D3',
-        },
-        lineStyle: { // 折线样式
-          color: { // 渐变色
-            type: 'linear',
-            x: 0,
-            y: 0,
-            x2: 0,
-            y2: 1,
-            colorStops: [
-              {
-                offset: 0, color: '#8700b3', // 顶部颜色
-              },
-              {
-                offset: 0.5, color: '#BA55D3',
-              },
-              {
-                offset: 1, color: '#fbccff', // 底部颜色
-              }
-            ],
-            global: false,
-          },
-        },
-        markLine: {
-          data: [
-            { // 中位数标线
-              type: 'median',
-              name: "中位数",
-              label: {
-                position: 'start',
-                formatter: "中位播放数",
-              },
-              lineStyle: {
-                color: '#00B2EE',
-              },
+        tooltip: { // 提示框
+            trigger: 'axis',
+            axisPointer: {
+                type: 'cross',
             },
-            [ // 最大值标线
-              { // 起点
-                symbol: 'circle',
-                x: '10%',
-                yAxis: 'max',
-                label: {
-                  position: 'start',
-                  formatter: "最多播放",
+            formatter: function (params) { // 自定义
+                /*
+                * 注意：
+                * params[0] 对应 series[0]，以此类推。
+                * 因为 series[0] 用了 data，
+                * 而 series[1|2] 用了 encode，
+                * 所以 params[0].value 返回 number，
+                * 而 params[1|2].value 返回 object。
+                */
+
+                let htmlStr = '';
+
+                htmlStr += params[1].name + "</br>"; // EP 数，与 X 轴相同
+                htmlStr += params[1].value['Title'] + "</br>"; // 标题
+                // htmlStr += params[1].value['Time'] + "</br>"; // 投稿时间
+
+                params.forEach((param) => {
+                    htmlStr += param.marker + " " + param.seriesName + " : ";
+                    if (param.value.constructor === Object) {
+                        let param_key = Object.keys(param.value)[param.encode['y'][0]];
+                        htmlStr += param.value[param_key];
+                    }
+                    else {
+                        htmlStr += param.value;
+                    }
+                    htmlStr += "</br>";
+                });
+
+                return htmlStr;
+            }
+        },
+        toolbox: { // 工具栏
+            right: '15%',
+            feature: {
+                dataView: {
+                    readOnly: true,
                 },
-              },
-              { // 终点
-                type: 'max',
-                name: "最高点",
-                symbol: 'none',
-                lineStyle: {
-                  color: '#00B2EE',
+                magicType: {
+                    type: ['line', 'bar'],
                 },
-              },
-            ],
-          ],
-        },
-      },
-      {
-        name: '弹幕数量',
-        type: 'bar',
-        yAxisIndex: 1,
-        encode: {
-          x: "EP",
-          y: "Danmaku",
-        },
-        itemStyle: {
-          color: '#66CD00',
-        },
-        markPoint: { // 最大值标点
-          symbolSize: 35,
-          data: [
-            {
-              type: 'max',
-              name: '最多弹幕数',
+                saveAsImage: {
+                    name: "【乃木坂工事中】千葉幽羽部分视频数据",
+                    pixelRatio: 1,
+                    backgroundColor: { // 下载水印
+                        type: 'pattern',
+                        image: getWaterMark(),
+                        repeat: 'repeat',
+                    },
+                },
+                restore: {},
             },
-          ],
         },
-      },
-      {
-        name: '评论数量',
-        type: 'bar',
-        yAxisIndex: 2,
-        encode: {
-          x: "EP",
-          y: "Comment",
+        grid: { // 直角坐标系
+            right: '10%',
+            // containLabel: true,
         },
-        itemStyle: {
-          color: '#EEC900',
+        legend: { // 图例
+            left: '10%',
+            data: ["播放次数", "弹幕数量", "评论数量"],
         },
-        markPoint: { // 最大值标点
-          symbolSize: 35,
-          data: [
-            {
-              type: 'max',
-              name: '最多评论数',
+        dataset: { // 数据集
+            source: data,
+        },
+        calculable: true,
+        xAxis: { // X 轴设置
+            // name: "期数",
+            type: 'category',
+            axisTick: {
+                alignWithLabel: true, // 刻度线与标签对齐
             },
-          ],
+            axisPointer: {
+                type: 'shadow',
+            },
         },
-      },
-    ],
-  };
+        yAxis: [ // Y 轴设置
+            {
+                name: "播放数",
+                type: 'value',
+                min: 0,
+                max: max_play,
+                interval: interval_play,
+                position: 'left',
+                axisLine: {
+                    show: true, // 显示轴线
+                },
+                axisLabel: {
+                    formatter: function (value) {
+                        value = +value;
+                        return isFinite(value) ? echarts.format.addCommas(+value / 1000) + 'K' : '';
+                    }
+                },
+            },
+            {
+                name: "弹幕数",
+                type: 'value',
+                min: 0,
+                max: max_danmaku,
+                interval: interval_danmaku,
+                position: 'right',
+                axisLine: {
+                    show: true,
+                },
+            },
+            {
+                name: "评论数",
+                type: 'value',
+                min: 0,
+                max: max_comment,
+                interval: interval_comment,
+                position: 'right',
+                offset: 70,
+                axisLine: {
+                    show: true,
+                },
+            },
+        ],
+        dataZoom: [ // 缩放
+            {
+                type: 'slider',
+                show: true,
+                start: 0,
+                end: 10,
+            },
+            {
+                // type: 'inside',
+                // show: true,
+                // start: 0,
+                // end: 25,
+            }
+        ],
+        series: [
+            {
+                name: '播放次数',
+                type: 'line',
+                yAxisIndex: 0,
+                // encode: {
+                //   x: "EP",
+                //   y: "Play",
+                // },
+                data: data.map(function (item) {
+                    return item["Play"];
+                }),
+                itemStyle: {  // 折线拐点样式
+                    color: '#BA55D3',
+                },
+                lineStyle: { // 折线样式
+                    color: { // 渐变色
+                        type: 'linear',
+                        x: 0,
+                        y: 0,
+                        x2: 0,
+                        y2: 1,
+                        colorStops: [
+                            {
+                                offset: 0, color: '#8700b3', // 顶部颜色
+                            },
+                            {
+                                offset: 0.5, color: '#BA55D3',
+                            },
+                            {
+                                offset: 1, color: '#fbccff', // 底部颜色
+                            }
+                        ],
+                        global: false,
+                    },
+                },
+                markLine: {
+                    data: [
+                        { // 中位数标线
+                            type: 'median',
+                            name: "中位数",
+                            label: {
+                                position: 'start',
+                                formatter: "中位播放数",
+                            },
+                            lineStyle: {
+                                color: '#00B2EE',
+                            },
+                        },
+                        [ // 最大值标线
+                            { // 起点
+                                symbol: 'circle',
+                                x: '10%',
+                                yAxis: 'max',
+                                label: {
+                                    position: 'start',
+                                    formatter: "最多播放",
+                                },
+                            },
+                            { // 终点
+                                type: 'max',
+                                name: "最高点",
+                                symbol: 'none',
+                                lineStyle: {
+                                    color: '#00B2EE',
+                                },
+                            },
+                        ],
+                    ],
+                },
+            },
+            {
+                name: '弹幕数量',
+                type: 'bar',
+                yAxisIndex: 1,
+                encode: {
+                    x: "EP",
+                    y: "Danmaku",
+                },
+                itemStyle: {
+                    color: '#66CD00',
+                },
+                markPoint: { // 最大值标点
+                    symbolSize: 35,
+                    data: [
+                        {
+                            type: 'max',
+                            name: '最多弹幕数',
+                        },
+                    ],
+                },
+            },
+            {
+                name: '评论数量',
+                type: 'bar',
+                yAxisIndex: 2,
+                encode: {
+                    x: "EP",
+                    y: "Comment",
+                },
+                itemStyle: {
+                    color: '#EEC900',
+                },
+                markPoint: { // 最大值标点
+                    symbolSize: 35,
+                    data: [
+                        {
+                            type: 'max',
+                            name: '最多评论数',
+                        },
+                    ],
+                },
+            },
+        ],
+    };
 
-  // 显示图表
-  myChart.setOption(option);
+    // 显示图表
+    myChart.setOption(option);
 
-  // 点击事件
-  myChart.on('click', function (params) {
-    // 点击折线拐点，柱状图跳转至相关网页
-    let url = "https://www.bilibili.com/video/";
-    if (params.componentType === "series"){
-      if (params.seriesType === "bar") {
-        let bv_id = params.data["BV"];
-        window.open(url + bv_id);
-      }
-      else if (params.seriesType === "line") {
-        let index = params.dataIndex;
-        let bv_id = data.map((item) => {return item["BV"];})[index];
-        window.open(url + bv_id);
-      }
-    }
-  });
+    // 点击事件
+    myChart.on('click', function (params) {
+        // 点击折线拐点，柱状图跳转至相关网页
+        let url = "https://www.bilibili.com/video/";
+        if (params.componentType === "series"){
+            if (params.seriesType === "bar") {
+                let bv_id = params.data["BV"];
+                window.open(url + bv_id);
+            }
+            else if (params.seriesType === "line") {
+                let index = params.dataIndex;
+                let bv_id = data.map((item) => {return item["BV"];})[index];
+                window.open(url + bv_id);
+            }
+        }
+    });
 }
 
--- src/js/fetch.js
+++ src/js/fetch.js
@@ -3,20 +3,46 @@
 import {drawChart} from "./container.js";
 import {drawChart2} from "./container2.js";
 
+let urls = [
+    'resources/bv_info.json',
+    'resources/bv_info2.json',
+];
+
 async function fetchURLs() {
-  try {
-    return await Promise.all([
-      fetch('resources/bv_info.json')
-          .then((res) => res.json()),
-      fetch('resources/bv_info2.json')
-          .then((res) => res.json()),
-    ]);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        return await Promise.allSettled(
+            urls.map(
+                (url) => fetch(url)
+                    .then((res) => res.json())
+            )
+        );
+        // return await Promise.all([
+        //     fetch('resources/bv_info.json')
+        //         .then((res) => res.json()),
+        //     fetch('resources/bv_info2.json')
+        //         .then((res) => res.json()),
+        // ]);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 fetchURLs().then((data) => {
-  drawChart(data[0]);
-  drawChart2(data[1]);
+    // Promise.all
+    // drawChart(data[0]);
+    // drawChart2(data[1]);
+
+    // Promise.allSettled
+    if (data[0].status === "fulfilled") {
+        drawChart(data[0].value);
+    } else {
+        // rejected
+        console.log(data[0].reason);
+    }
+    if (data[1].status === "fulfilled") {
+        drawChart2(data[1].value);
+    } else {
+        // rejected
+        console.log(data[1].reason);
+    }
 });
