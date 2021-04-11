561195cca210ec8eb692272a045f5d774d02691e
Kasen <18170166+imkasen@users.noreply.github.com>
2021-04-11 22:24:08 +0800

    调整弹幕字体

--- src/js/container3.js
+++ src/js/container3.js
@@ -27,7 +27,7 @@ export function drawChart3(data) {
             height: '100%',
             // right: null,
             // bottom: null,
-            sizeRange: [20, 200],
+            sizeRange: [15, 150],
             rotationRange: [-90, 90],
             rotationStep: 30,
             gridSize: 5,
