b47d45cbd58b7fc95510ccf5e5df81c5abe61496
Kasen <18170166+imkasen@users.noreply.github.com>
2021-04-23 21:36:39 +0800

    new theme

--- src/js/scroll-bak.js
+++ src/js/scroll-bak.js
@@ -4,9 +4,9 @@
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
-            location.pathname.replace(/^\//, "") ===
-            this.pathname.replace(/^\//, "") &&
-            location.hostname === this.hostname
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
         ) {
             let target = $(this.hash);
             target = target.length
@@ -15,7 +15,7 @@
             if (target.length) {
                 $("html, body").animate(
                     {
-                        scrollTop: target.offset().top - 70,
+                        scrollTop: target.offset().top,
                     },
                     1000,
                     "easeInOutExpo"
@@ -32,20 +32,6 @@
 
     // Activate scrollspy to add active class to navbar items on scroll
     $("body").scrollspy({
-        target: "#mainNav",
-        offset: 100,
+        target: "#sideNav",
     });
-
-    // Collapse Navbar
-    const navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
